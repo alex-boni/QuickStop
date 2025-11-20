4cd96ef72d7c08518644f44173190967b1323da0
airamsoto <airamxl9@gmail.com>
2025-11-20 19:59:02 +0100

    eliminado imports no necesarios

--- api/src/main/java/es/quickstop/api/parking/ParkingController.java
+++ api/src/main/java/es/quickstop/api/parking/ParkingController.java
@@ -7,11 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 
 import es.quickstop.api.parking.dto.ParkingDTO;
 import java.util.List;
-import java.util.ArrayList;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
